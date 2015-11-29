4c3cb88a39801a5ccbf83c76ff86490ca8720ce3
mahmoudHussein <m.hussein0793@gmail.com>
2015-11-29 22:40:13 +0100

    added the publisher fuser

--- DataFusion/src/main/java/de/uni_mannheim/informatik/wdi/datafusion/usecase/books/fusers/PublisherFuser.java
+++ DataFusion/src/main/java/de/uni_mannheim/informatik/wdi/datafusion/usecase/books/fusers/PublisherFuser.java
@@ -1,5 +1,38 @@
 package de.uni_mannheim.informatik.wdi.datafusion.usecase.books.fusers;
 
-public class PublisherFuser {
+import de.uni_mannheim.informatik.wdi.datafusion.AttributeValueFuser;
+import de.uni_mannheim.informatik.wdi.datafusion.FusedValue;
+import de.uni_mannheim.informatik.wdi.datafusion.RecordGroup;
+import de.uni_mannheim.informatik.wdi.datafusion.conflictresolution.string.LongestString;
+import de.uni_mannheim.informatik.wdi.datafusion.usecase.books.FusableBooks;
+
+public class PublisherFuser extends AttributeValueFuser<String, FusableBooks>{
+
+	public PublisherFuser() {
+		super(new LongestString<FusableBooks>());
+	}
+
+	public void fuse(RecordGroup<FusableBooks> group,
+			FusableBooks fusedRecord) {
+		
+		// get the fused value
+		FusedValue<String, FusableBooks> fused = getFusedValue(group);
+		
+		// set the value for the fused record
+		fusedRecord.setPublisher(fused.getValue());
+		
+		// add provenance info
+		fusedRecord.setAttributeProvenance(FusableBooks.PUBLISHER, fused.getOriginalIds());
+	}
+
+	@Override
+	public boolean hasValue(FusableBooks record) {
+		return record.hasValue(FusableBooks.PUBLISHER);
+	}
+	
+	@Override
+	protected String getValue(FusableBooks record) {
+		return record.getPublisher();
+	}
 
 }
